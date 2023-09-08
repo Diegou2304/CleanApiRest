381ae3393e1a0729ad0ea9e1da6acd1bb1ebc84f
Diegou2304 <diegou2304-98@hotmail.com>
2023-09-08 11:07:02 -0400

    Added new result class

---
+++ CleanApiRest.Application/Cars/GetCars/GetCarsQueryResult.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace CleanApiRest.Application.Cars.GetCars
+{
+    public class GetCarsQueryResult : IRequest<IEnumerable<GetCarsQueryResult>>
+    {
+        public int CarId { get; set; }
+        public string Brand { get; set; }
+        public int CarStoreId { get; set; }
+        public string Color { get; set; }
+
+    }
+}
